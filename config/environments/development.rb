ee45ea1e05965dc9e61336ede90fe6f53897efa1
58784 <m7mkmnn@gmail.com>
2023-02-03 07:29:52 +0000

    パスワードリセット画面の装飾

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -67,7 +67,5 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
-  host = 'ap-southeast-1.amazonaws.com'
-  Rails.application.routes.default_url_options[:host] = host
   config.hosts.clear
 end
