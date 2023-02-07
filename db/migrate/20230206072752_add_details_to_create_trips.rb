8576890f3fafce81c3a6aebb232ced4cc048e11c
58784 <m7mkmnn@gmail.com>
2023-02-07 02:35:54 +0000

    余計なカラムを排除

--- db/migrate/20230206072752_add_details_to_create_trips.rb
+++ db/migrate/20230206072752_add_details_to_create_trips.rb
@@ -2,6 +2,5 @@ class AddDetailsToCreateTrips < ActiveRecord::Migration[7.0]
   def change
     add_column :trips, :name_of_trip, :string
     add_column :trips, :when, :date
-    add_column :trips, :total_day, :integer
   end
 end
